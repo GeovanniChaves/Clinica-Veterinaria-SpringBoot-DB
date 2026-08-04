85cb1cb016d2b2467d4fb44c92f675a8014996c5
GeovanniChaves <chavesgeovanni0@gmail.com>
2026-08-04 01:58:53 -0300

    feat: implementa consulta de tutores por id
    - Criado endpoint GET /tutores
    - Criado endpoint GET /tutores/{id}
    - Implementada busca de tutores utilizando JpaRepository
    - Integrado Controller com camada Repository

--- clinica-veterinaria/src/main/java/br/com/geovanni/clinicaveterinaria/controller/TutorController.java
+++ clinica-veterinaria/src/main/java/br/com/geovanni/clinicaveterinaria/controller/TutorController.java
@@ -1,5 +1,9 @@
 package br.com.geovanni.clinicaveterinaria.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -24,4 +28,16 @@ public class TutorController {
         return repository.save(tutor);
     }
 
+    @GetMapping("/{id}")
+    public Tutor buscarPorId(@PathVariable Long id) {
+        return repository.findById(id).get();
+    }
+    
+    @GetMapping
+    public List<Tutor> listar() {
+        return repository.findAll();
+    }
+    
+    
+    
 }
